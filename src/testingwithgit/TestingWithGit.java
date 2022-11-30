141fc375214f52dafe07094aa3eb749c6220ead3
LAPTOP-TPG8VJ51\wongj <wongjennhau@gmail.com>
2022-11-30 15:08:36 +0800

    new commit 3

--- src/testingwithgit/TestingWithGit.java
+++ src/testingwithgit/TestingWithGit.java
@@ -16,6 +16,11 @@ public class TestingWithGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        int i = 0;
+        if (i<100) {
+            System.out.println("Hello");
+            i++;
+        }
     }
     
 }
