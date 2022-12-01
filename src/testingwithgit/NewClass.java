ce65a70f3967e4eaacfa52e066b50168c12ad7f3
Jenn Hau <wongjennhau@gmail.com>
2022-12-01 20:19:48 +0800

    no message

---
+++ src/testingwithgit/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testingwithgit;
+
+/**
+ *
+ * @author wongj
+ */
+public class NewClass {
+    
+    public static void testing(String[] args) {
+        System.out.println("New Class Testing");
+    }
+}
